fbce07ef03d6884eefde1d5a0c295d6419bdd043
Ivan Francic <francicfrane@gmail.com>
2023-03-08 23:20:24 +0100

    Dodani Player Entity Creature klase

--- src/dev/codenmore/tilegame/states/GameState.java
+++ src/dev/codenmore/tilegame/states/GameState.java
@@ -1,23 +1,26 @@
 package dev.codenmore.tilegame.states;
 
+import dev.codenmore.tilegame.entities.creatures.Player;
 import dev.codenmore.tilegame.gfx.Assets;
 
 import java.awt.*;
 
 public class GameState extends State {
 
-        public GameState() {
+        private Player player;
 
+        public GameState() {
+                player = new Player(100, 100);
         }
 
         @Override
         public void tick() {
-
+                player.tick();
         }
 
         @Override
         public void render(Graphics g) {
-                
+                player.render(g);
         }
 
 //        test koment
