e02a185bba81a16c31f8f356352224e89f8b03f6
francicfrane@gmail.com <francicfrane@gmail.com>
2023-03-08 22:52:29 +0100

    fix (tu je dodan menu)

--- src/dev/codenmore/tilegame/states/MenuState.java
+++ src/dev/codenmore/tilegame/states/MenuState.java
@@ -1,2 +1,18 @@
-package dev.codenmore.tilegame.states;public class MenuState {
+package dev.codenmore.tilegame.states;
+
+import dev.codenmore.tilegame.gfx.Assets;
+
+import java.awt.*;
+
+public class MenuState extends State {
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(Assets.player, 0, 0 , null);
+    }
 }
