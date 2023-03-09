642983a84f13f13b45bf94374f8bf6e5ec6c1d80
Ivan Francic <francicfrane@gmail.com>
2023-03-09 21:20:16 +0100

    Dodana promjena veličine creaturea, dodani getteri/setteri, dorada movment-a

--- src/dev/codenmore/tilegame/entities/creatures/Creature.java
+++ src/dev/codenmore/tilegame/entities/creatures/Creature.java
@@ -4,10 +4,58 @@ import dev.codenmore.tilegame.entities.Entity;
 
 public abstract class Creature extends Entity {
 
-    protected int health;
+    public static final int DEFAULT_HEALTH = 10;
+    public static final float DEFAULT_SPEED = 3.0f;
+    public static final int DEFAULT_CREATURE_WIDTH = 40, DEFAULT_CREATURE_HEIGHT = 40;
 
-    public Creature(float x, float y) {
-        super(x, y);
-        health = 10;
+    protected int health;
+    protected float speed, xMove, yMove;
+
+    public void move() {
+        x += xMove;
+        y += yMove;
+    }
+
+
+//    GETTERS SETTERS
+
+    public float getxMove() {
+        return xMove;
+    }
+
+    public void setxMove(float xMove) {
+        this.xMove = xMove;
+    }
+
+    public float getyMove() {
+        return yMove;
+    }
+
+    public void setyMove(float yMove) {
+        this.yMove = yMove;
+    }
+
+    public Creature(float x, float y, int width, int height) {
+        super(x, y, width, height);
+        health = DEFAULT_HEALTH;
+        speed = DEFAULT_SPEED;
+        xMove = 0;
+        yMove = 0;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
     }
 }
