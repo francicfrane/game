a37e8434e4a941f4cf93271d0b276b3443fd352c
Ivan Francic <francicfrane@gmail.com>
2023-03-10 20:00:29 +0100

    Dodana klasa world

--- src/dev/codenmore/tilegame/states/GameState.java
+++ src/dev/codenmore/tilegame/states/GameState.java
@@ -4,27 +4,32 @@ import dev.codenmore.tilegame.Game;
 import dev.codenmore.tilegame.entities.creatures.Player;
 import dev.codenmore.tilegame.gfx.Assets;
 import dev.codenmore.tilegame.tiles.Tile;
+import dev.codenmore.tilegame.worlds.World;
 
 import java.awt.*;
 
 public class GameState extends State {
 
         private Player player;
+        private World world;
 
         public GameState(Game game) {
                 super(game);
                 player = new Player(game,100, 100);
+                world = new World("hey");
         }
 
         @Override
         public void tick() {
+                world.tick();
                 player.tick();
         }
 
         @Override
         public void render(Graphics g) {
+                world.render(g);
                 player.render(g);
-                Tile.tiles[0].render(g, 0, 0);
+//                Tile.tiles[0].render(g, 0, 0);
         }
 
 //        test koment
