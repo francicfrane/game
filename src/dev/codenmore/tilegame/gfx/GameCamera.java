034d52837982c1d1374bcf55f623527c272e6e7d
Ivan Francic <francicfrane@gmail.com>
2023-03-15 20:51:16 +0100

    Dodan kamera limit (scrola se rubova samo, ali ne i prek)

--- src/dev/codenmore/tilegame/gfx/GameCamera.java
+++ src/dev/codenmore/tilegame/gfx/GameCamera.java
@@ -1,32 +1,35 @@
 package dev.codenmore.tilegame.gfx;
 
-import dev.codenmore.tilegame.Game;
+import dev.codenmore.tilegame.Handler;
 import dev.codenmore.tilegame.entities.Entity;
+import dev.codenmore.tilegame.tiles.Tile;
 
 public class GameCamera {
 
     private float xOffset, yOffset;
-    Game game;
+    private Handler handler;
 
-    public GameCamera(Game game, float xOffset, float yOffset) {
-        this.game = game;
+    public GameCamera(Handler handler, float xOffset, float yOffset) {
+        this.handler = handler;
         this.xOffset = xOffset;
         this.yOffset = yOffset;
     }
 
     public void centerOnEntity(Entity e) {
-        xOffset = e.getX() - game.getWidth() / 2 + e.getWidth() / 2;
-        yOffset = e.getY() - game.getHeight() / 2 + e.getHeight() / 2;
+        xOffset = e.getX() - handler.getWidth() / 2 + e.getWidth() / 2;
+        yOffset = e.getY() - handler.getHeight() / 2 + e.getHeight() / 2;
+        checkBlackSpace();
     }
 
     public void move(float xAmt, float yAmt) {
         xOffset += xAmt;
         yOffset += yAmt;
+        checkBlackSpace();
     }
 
     public void center(Entity e) {
-        xOffset = e.getX() - game.getWidth() / 2;
-        yOffset = e.getY() - game.getHeight() / 2;
+        xOffset = e.getX() - handler.getWidth() / 2;
+        yOffset = e.getY() - handler.getHeight() / 2;
     }
 
     public float getxOffset() {
@@ -43,5 +46,19 @@ public class GameCamera {
 
     public void setyOffset(float yOffset) {
         this.yOffset = yOffset;
+    }
+
+    public void checkBlackSpace() {
+        if (xOffset < 0) {
+            xOffset = 0;
+        } else if(xOffset > handler.getWorld().getWidth() * Tile.TILEWIDTH - handler.getWidth()) {
+            xOffset = handler.getWorld().getWidth() * Tile.TILEWIDTH - handler.getWidth();
+        }
+
+        if (yOffset < 0) {
+            yOffset = 0;
+        } else if(yOffset > handler.getWorld().getHeight() * Tile.TILEHEIGHT - handler.getWidth()) {
+            yOffset = handler.getWorld().getHeight() * Tile.TILEHEIGHT - handler.getWidth();
+        }
     }
 }
